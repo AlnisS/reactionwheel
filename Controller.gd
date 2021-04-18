8f951fbade5ee17a7549bf121d16ed5a50fa1e51
Alnis Smidchens <alnis.smidchens@gmail.com>
2021-04-18 01:46:54 -0700

    PID controller

--- Controller.gd
+++ Controller.gd
@@ -2,25 +2,85 @@ extends Node
 
 
 # System constants
-const J_STAND = 5.0
-const J_FLYWHEEL = 5.0
-const FRICTION_BEARING = 0.4
-const ANGULAR_VELOCITY_TO_ESC_OUTPUT = 4.0
+#const J_STAND = 5.0
+#const J_FLYWHEEL = 5.0
+#const FRICTION_BEARING = 0.4
+#const ANGULAR_VELOCITY_TO_ESC_OUTPUT = 4.0
+
+const J_STAND = 0.00113204  # from CAD, kg * m^2
+const J_FLYWHEEL = 0.00108912 / 15.0 # from CAD, kg * m^2
+const FRICTION_BEARING = 0.0005  # guessed, N * m
+#const FRICTION_MOTOR = 0.003  # guessed, N * m
+const MOTOR_STALL_TORQUE = 0.1079  # 8.3 * 13 A / 1000 kv, N * m
+const MOTOR_KV = 1000.0  # from motor datasheet
+const MAX_VOLTAGE = 12.0  # from motor datasheet
+
+const ANGULAR_VELOCITY_TO_ESC_OUTPUT = 1.0 / (MOTOR_KV * MAX_VOLTAGE * 2 * PI)
+
+const P = 0.3
+const I = 0.1
+const D = 0.0
 
 # Initial sensor measurements
 var velocity_stand: float = 0.0
 var velocity_flywheel: float = 0.0
 
 # Controller inputs
-var velocity_target: float = 15
+var velocity_target: float = 0.0
 
+var last_delta_v = 0
+var delta_v_i = 0.0
+
+var time := 0.0
 
 func _physics_process(delta):
+#	update_sensors()
+#	var momentum_stand = velocity_stand * J_STAND
+#	var momentum_flywheel = velocity_flywheel * J_FLYWHEEL
+#	var momentum_stand_target = velocity_target * J_STAND
+#
+#	var delta_momentum_stand_target = momentum_stand_target - momentum_stand
+#	var momentum_flywheel_target = delta_momentum_stand_target + momentum_flywheel
+#	var velocity_flywheel_target = momentum_flywheel_target / J_FLYWHEEL
+#
+#	var esc_output = -ANGULAR_VELOCITY_TO_ESC_OUTPUT * velocity_flywheel_target
+#	esc_write(esc_output)
+	
+	velocity_target = PI * sin(time * 0.2)
+	
 	update_sensors()
+	
+	# error
+	var delta_v = velocity_target - velocity_stand
+	# change in error divided by time step = derivative
+	var delta_delta_v = (last_delta_v - delta_v) / delta
+	# integrate error
+	delta_v_i += delta_v * delta
+	
+	last_delta_v = delta_v
+	
+	
+	
+	var esc_output = delta_v * P + delta_v_i * I + delta_delta_v * D
+	
+	# motor spins opposite direction from stand velocity
+	esc_output = -esc_output
+	
+	print("delta_v: " + str(delta_v)
+			+ "\ndelta_delta_v: " + str(delta_delta_v)
+			+ "\ndelta_v_i: " + str(delta_v_i)
+			+ "\nesc_output: " + str(esc_output)
+			+ "\n")
+	
+	esc_write(esc_output)
+
+	time += delta
+	
 
 func update_sensors():
-	pass
+	velocity_stand = get_parent().velocity_stand
+	velocity_flywheel = get_parent().velocity_flywheel
 
-func esc_write():
-	pass
+func esc_write(esc_output):
+	get_parent().esc_input = clamp(esc_output, -1, 1)
 
--- TestStand.gd
+++ TestStand.gd
@@ -1,9 +1,10 @@
 extends Spatial
 
 const J_STAND = 0.00113204  # from CAD, kg * m^2
-const J_FLYWHEEL = 0.00108912  # from CAD, kg * m^2
+const J_FLYWHEEL = 0.00108912 / 15.0 # from CAD, kg * m^2
 const FRICTION_BEARING = 0.0005  # guessed, N * m
-const FRICTION_MOTOR = 0.003  # guessed, N * m
+#const FRICTION_MOTOR = 0.003  # guessed, N * m
+const FRICTION_MOTOR = 0.0
 const MOTOR_STALL_TORQUE = 0.1079  # 8.3 * 13 A / 1000 kv, N * m
 const MOTOR_KV = 1000.0  # from motor datasheet
 const MAX_VOLTAGE = 12.0  # from motor datasheet
@@ -15,39 +16,71 @@ var momentum_stand = 0.0
 var momentum_flywheel = 0.0
 
 var motor_power = 0.0  # on a scale of 0.0 to 1.0 (0% to 100%)
+var motor_torque = 0.0
 var time = 0
 
+# Value comes from attached controller
+var esc_input := 0.0
+
 func _ready():
-	heavy()
-#	light()
-
-
+#	heavy()
+	light()
 
 
 func _physics_process(delta):
 	
+	_update_motor_power()
+	_update_motor_torque()
+	
 	_update_momentum()
 	
 	_apply_motor_impulse(delta)
-	_apply_motor_friction(delta)
 	_apply_stand_friction(delta)
+	_apply_motor_friction(delta)
+	
 	
 	_update_velocity()
 	_update_rotation(delta)
 	
-	_update_motor_power()
+	get_node("../Label").text = (
+			"Stand velocity: " + str(velocity_stand) +
+			"\nTarget velocity: " + str($Controller.velocity_target) +
+			"\nVelocity error: " + str(velocity_stand - $Controller.velocity_target) +
+			"\nFlywheel velocity: " + str(velocity_flywheel))
 	
 	time += delta
 
 
 # physics update functions -----------------------------------------------------
 
+func _update_motor_power():
+#	motor_power = 0.0
+#
+#	if Input.is_key_pressed(KEY_SPACE):
+#		motor_power = cos(time / 1) * 0.1
+#
+#	if Input.is_key_pressed(KEY_D):
+#		motor_power = 0.5
+#
+#	if Input.is_key_pressed(KEY_A):
+#		motor_power = -0.5
+	motor_power = esc_input
+
+func _update_motor_torque():
+	var voltage = MAX_VOLTAGE * motor_power
+	var free_rpm = abs(voltage) * MOTOR_KV
+	var stall_torque = MOTOR_STALL_TORQUE * abs(motor_power)
+	var current_rpm = abs((velocity_flywheel - velocity_stand) / (2 * PI) * 60.0)
+	motor_torque = 0.0
+	if (current_rpm < free_rpm):
+		motor_torque = lerp(stall_torque, 0, current_rpm / free_rpm)
+
 func _update_momentum():
 	momentum_stand = velocity_stand * J_STAND
 	momentum_flywheel = velocity_flywheel * J_FLYWHEEL
 
 func _apply_motor_impulse(delta: float):
-	var motor_impulse = motor_power * MOTOR_STALL_TORQUE * delta * 0.1
+	var motor_impulse = motor_power * motor_torque * delta
 	momentum_stand -= motor_impulse
 	momentum_flywheel += motor_impulse
 
@@ -62,6 +95,8 @@ func _apply_motor_friction(delta: float):
 
 func _apply_stand_friction(delta: float):
 	momentum_stand = derp(momentum_stand, 0, FRICTION_BEARING * delta)
+	# air drag like friction to slow it down when going really fast
+	momentum_stand -= 0.2 * pow(momentum_stand, 2) * sign(momentum_stand)
 
 func _update_velocity():
 	velocity_stand = momentum_stand / J_STAND
@@ -70,18 +105,6 @@ func _update_velocity():
 func _update_rotation(delta: float):
 	$RotatingStand.rotate_z(velocity_stand * delta)
 	$Flywheel.rotate_z(velocity_flywheel * delta)
-
-func _update_motor_power():
-	motor_power = 0.0
-	
-	if Input.is_key_pressed(KEY_SPACE):
-		motor_power = cos(time / 1) * 0.5
-	
-	if Input.is_key_pressed(KEY_D):
-		motor_power = 1.0
-	
-	if Input.is_key_pressed(KEY_A):
-		motor_power = -1.0
 
 # utility functions ------------------------------------------------------------
 
